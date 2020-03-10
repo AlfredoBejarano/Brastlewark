711fe48d142f0f51085632fd6369192c5057680d
Alfredo Bejarano <alfredocbejarano@gmail.com>
2020-03-10 17:17:36 -0600

    Update SplashActivity.kt
    Move the setting of the image bitmap to the UI thread

--- app/src/main/java/me/alfredobejarano/brastlewark/SplashActivity.kt
+++ app/src/main/java/me/alfredobejarano/brastlewark/SplashActivity.kt
@@ -2,6 +2,8 @@ package me.alfredobejarano.brastlewark
 
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager.LayoutParams
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import me.alfredobejarano.brastlewark.datasource.network.NetworkAdapter
 
@@ -10,10 +12,22 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        NetworkAdapter.getJSONResource({
-            it.forEachIndexed { index, gnome ->
-                Log.d("Gnome #$index", gnome.toString())
-            }
+        val imageView = ImageView(this)
+        windowManager?.addView(
+            imageView, LayoutParams(
+                LayoutParams.TYPE_APPLICATION,
+                LayoutParams.FLAG_NOT_FOCUSABLE
+            )
+        )
+
+        NetworkAdapter.getJSONResource({ list ->
+            val imageSrc = list.first().thumbnailUrl
+            Log.d("IMAGE", imageSrc)
+            NetworkAdapter.getBitmapFromURL(imageSrc, {
+                runOnUiThread { imageView.setImageBitmap(it) }
+            }, {
+                it.printStackTrace()
+            })
         }, {
             it.printStackTrace()
         })
