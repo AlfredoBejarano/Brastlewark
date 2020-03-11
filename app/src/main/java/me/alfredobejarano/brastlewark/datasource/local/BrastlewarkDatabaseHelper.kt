c1a959ef4b5ff8beca2ca24572429067e44b9e0a
Alfredo Bejarano <alfredocbejarano@gmail.com>
2020-03-10 18:33:18 -0600

    Create BrastlewarkDatabaseHelper.kt

---
+++ app/src/main/java/me/alfredobejarano/brastlewark/datasource/local/BrastlewarkDatabaseHelper.kt
@@ -0,0 +1,40 @@
+package me.alfredobejarano.brastlewark.datasource.local
+
+import android.app.Application
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BrastlewarkDatabase private constructor(ctx: Context) :
+    SQLiteOpenHelper(ctx, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_NAME = "brastlewark.db"
+        private const val DATABASE_VERSION = 1
+
+        private var INSTANCE: BrastlewarkDatabase? = null
+
+        fun getInstance(app: Application) = INSTANCE ?: synchronized(this) {
+            INSTANCE ?: createInstance(app).also { INSTANCE = it }
+        }
+
+        private fun createInstance(app: Application) = BrastlewarkDatabase(app)
+    }
+
+    private lateinit var gnomeDataBaseHelper: GnomeDataBaseHelper
+    private lateinit var cachedPhotoDataBaseHelper: CachedPhotoDataBaseHelper
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        gnomeDataBaseHelper = GnomeDataBaseHelper(db).also { it.createTable() }
+        cachedPhotoDataBaseHelper = CachedPhotoDataBaseHelper(db).also { it.createTable() }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        GnomeDataBaseHelper(db).nukeTable()
+        CachedPhotoDataBaseHelper(db).nukeTable()
+        onCreate(db)
+    }
+
+    fun getGnomeDataBaseHelper() = gnomeDataBaseHelper
+    fun getCachedPhotoDataBaseHelper() = cachedPhotoDataBaseHelper
+}
