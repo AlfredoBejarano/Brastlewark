73203686c0c7e334a114bcefbe1ab7ee1c08f9a5
Alfredo Bejarano <alfredocbejarano@gmail.com>
2020-03-10 19:51:58 -0600

    Update GnomeRepository.kt

--- app/src/main/java/me/alfredobejarano/brastlewark/repository/GnomeRepository.kt
+++ app/src/main/java/me/alfredobejarano/brastlewark/repository/GnomeRepository.kt
@@ -4,6 +4,7 @@ import me.alfredobejarano.brastlewark.datasource.local.GnomeDataBaseHelper
 import me.alfredobejarano.brastlewark.datasource.local.SharedPreferencesDataSource
 import me.alfredobejarano.brastlewark.datasource.network.GnomeApiService
 import me.alfredobejarano.brastlewark.model.Gnome
+import me.alfredobejarano.brastlewark.utils.runOnWorkerThread
 
 class GnomeRepository(
     private val remoteDataSource: GnomeApiService,
@@ -27,9 +28,20 @@ class GnomeRepository(
     }
 
     fun getGnomes(onComplete: (result: Pair<List<Gnome>?, Exception?>) -> Unit) =
-        if (sharedPreferencesDataSource.isGnomeCacheValid()) {
-            getGnomesByCache(onComplete)
-        } else {
-            getGnomesByNetwork(onComplete)
+        runOnWorkerThread {
+            if (sharedPreferencesDataSource.isGnomeCacheValid()) {
+                getGnomesByCache(onComplete)
+            } else {
+                getGnomesByNetwork(onComplete)
+            }
+        }
+
+    fun getGnomeByName(name: String, onComplete: (result: Pair<Gnome?, Exception?>) -> Unit) =
+        runOnWorkerThread {
+            try {
+                onComplete(Pair(localDataSource.getGnomeByName(name), null))
+            } catch (e: Exception) {
+                onComplete(Pair(null, e))
+            }
         }
 }
